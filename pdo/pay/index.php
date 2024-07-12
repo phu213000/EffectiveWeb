4435029627d805f081e9cdf62cdd932451713b70
Hồ Trường Minh Phú <117896798+phu213000@users.noreply.github.com>
2024-07-12 14:59:02 +0700

    upload day 3

--- pdo/pay/index.php
+++ pdo/pay/index.php
@@ -79,4 +79,4 @@ if ($conn) {
     </div>
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
       © 2023 Bản quyền:
-      <a class="text-white" href="https://mdbootstrap.com/">MDB
+      <a class="text-white" href="https://mdbootstrap.com/">Ho Truong Minh Phu
